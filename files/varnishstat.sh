5bee3e1f9aa20a90063a329b2689dd54c68de632
Carlos Abalde <carlos.abalde@gmail.com>
2025-02-04 18:18:07 +0100

    Cosmetic

--- files/varnishstat.sh
+++ files/varnishstat.sh
@@ -25,54 +25,54 @@
 #   - Beware of JSON numbers being limited to float64 & possible precision loss.
 #
 
-COUNTERS=$(cat <<EOF
-"MGT.uptime": {
-  "description": "Management process uptime",
-  "flag": "c",
-  "format": "d",
-  "value": 6060502
-},
-"MAIN.n_backend": {
-  "description": "Number of backends",
-  "flag": "g",
-  "format": "i",
-  "value": 6
-},
-"VBE.boot.default_be.happy": {
-  "description": "Happy health probes",
-  "flag": "b",
-  "format": "b",
-  "value": 18446744073709551615
-},
-"VBE.boot.default_be.is_healthy": {
-  "description": "Backend health status",
-  "flag": "q",
-  "format": "q",
-  "value": 1
-},
-"MAIN.s_req_hdrbytes": {
-  "description": "Request header bytes",
-  "flag": "c",
-  "format": "B",
-  "value": 854488484
-}
-EOF
-)
+COUNTERS='
+  "MGT.uptime": {
+    "description": "Management process uptime",
+    "flag": "c",
+    "format": "d",
+    "value": 6060502
+  },
+  "MAIN.n_backend": {
+    "description": "Number of backends",
+    "flag": "g",
+    "format": "i",
+    "value": 6
+  },
+  "VBE.boot.default_be.happy": {
+    "description": "Happy health probes",
+    "flag": "b",
+    "format": "b",
+    "value": 18446744073709551615
+  },
+  "VBE.boot.default_be.is_healthy": {
+    "description": "Backend health status",
+    "flag": "q",
+    "format": "q",
+    "value": 1
+  },
+  "MAIN.s_req_hdrbytes": {
+    "description": "Request header bytes",
+    "flag": "c",
+    "format": "B",
+    "value": 854488484
+  }'
 
 if [ $((RANDOM % 2)) -eq 0 ]; then
-  cat <<EOF
+  # Old 'varnishstat' output.
+  jq <<EOF
   {
     "timestamp": "2024-01-01T13:00:00",
-$(echo "$COUNTERS" | sed 's/^/    /')
+    $COUNTERS
   }
 EOF
 else
-  cat <<EOF
+  # New 'varnishstat' output, for Varnish Cache >= 6.5.0.
+  jq <<EOF
   {
     "version": 1,
     "timestamp": "2024-01-01T13:00:00",
     "counters": {
-$(echo "$COUNTERS" | sed 's/^/      /')
+      $COUNTERS
     }
   }
 EOF
